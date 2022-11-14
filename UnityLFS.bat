168758f873194cf361bf36f186064301fede081a
Johann Ungerer <johannu@azurosolutions.com>
2022-11-14 09:49:55 +0200

    Added gitignore and set up LFS

---
+++ UnityLFS.bat
@@ -0,0 +1,70 @@
+@echo off
+
+rem Unity Packages & Files
+git lfs track "*.unitypackage"
+git lfs track "*.anim"
+
+rem 3D models
+git lfs track "*.3dm"
+git lfs track "*.3ds"
+git lfs track "*.blend"
+git lfs track "*.blend1"
+git lfs track "*.c4d"
+git lfs track "*.collada"
+git lfs track "*.dae"
+git lfs track "*.dxf"
+git lfs track "*.fbx"
+git lfs track "*.jas"
+git lfs track "*.lws"
+git lfs track "*.lxo"
+git lfs track "*.ma"
+git lfs track "*.max"
+git lfs track "*.mb"
+git lfs track "*.obj"
+git lfs track "*.ply"
+git lfs track "*.skp"
+git lfs track "*.stl"
+git lfs track "*.ztl"
+
+rem Audio
+git lfs track "*.aif"
+git lfs track "*.aiff"
+git lfs track "*.it"
+git lfs track "*.mod"
+git lfs track "*.mp3"
+git lfs track "*.ogg"
+git lfs track "*.s3m"
+git lfs track "*.wav"
+git lfs track "*.xm"
+
+rem Fonts
+git lfs track "*.otf"
+git lfs track "*.ttf"
+
+rem Images
+git lfs track "*.bmp"
+git lfs track "*.exr"
+git lfs track "*.gif"
+git lfs track "*.hdr"
+git lfs track "*.iff"
+git lfs track "*.jpeg"
+git lfs track "*.jpg"
+git lfs track "*.pict"
+git lfs track "*.png"
+git lfs track "*.psd"
+git lfs track "*.tga"
+git lfs track "*.tif"
+git lfs track "*.tiff"
+git lfs track "*.xd"
+
+rem Mobile Binaries
+git lfs track "*.apk"
+git lfs track "*.aab"
+git lfs track "*.app"
+
+rem Archives and Compressed Files
+git lfs track "*.zip"
+git lfs track "*.gz"
+git lfs track "*.7z"
+git lfs track "*.tar"
+git lfs track "*.rar"
