3740eabf2337d41e777043221561ab6e21417c69
Hamish <76265738+hbgr@users.noreply.github.com>
2022-11-28 12:20:58 +0000

    Added edges of game area and basic scoring

---
+++ EG_Prototype/Assets/Scripts/GameManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private int _score = 0;
+
+    [SerializeField]
+    private Text _scoreText;
+
+    void Awake()
+    {
+        if (instance == null && instance != this)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void Start()
+    {
+        _scoreText.text = "Score: " + _score;
+    }
+
+    public void ScorePoints(int points)
+    {
+        _score += points;
+        _scoreText.text = "Score: " + _score;
+    }
+}
