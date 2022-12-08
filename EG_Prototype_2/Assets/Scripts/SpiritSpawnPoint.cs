dccc2a510474f2a9bb3a1dad4ae4aa4177acb37d
Hamish <76265738+hbgr@users.noreply.github.com>
2022-12-08 11:38:36 +0000

    Simple implementation of spirit wave spawning

---
+++ EG_Prototype_2/Assets/Scripts/SpiritSpawnPoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritSpawnPoint : MonoBehaviour
+{
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, 1);
+    }
+}
