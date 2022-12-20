c8c9af4a604c5a7ce3d0cb9171ef32f228cc25cc
Hamish <76265738+hbgr@users.noreply.github.com>
2022-12-20 17:11:49 +0000

    Updated movement

--- EG_Prototype_2/Assets/Scripts/PlayerPhysicsController.cs
+++ EG_Prototype_2/Assets/Scripts/PlayerPhysicsController.cs
@@ -5,184 +5,191 @@ using UnityEngine.InputSystem;
 
 public class PlayerPhysicsController : MonoBehaviour
 {
-	[SerializeField]
-	private float _acceleration = 20.0f;
+    [SerializeField]
+    private float _acceleration = 20.0f;
 
-	[SerializeField]
-	private float _maxSpeed = 6.0f;
+    [SerializeField]
+    private float _maxSpeed = 6.0f;
 
-	//	Degrees to turn per second
-	[SerializeField]
-	private float _turnSpeed = 90.0f;
+    //	Degrees to turn per second
+    [SerializeField]
+    private float _turnSpeed = 90.0f;
 
-	[SerializeField]
-	private float _interactionRadius = 2.0f;
+    [SerializeField]
+    private float _interactionRadius = 2.0f;
 
-	//	Should probably not be here
-	[SerializeField]
-	private float _maxAnimationSpeed = 4.0f;
+    //	Should probably not be here
+    [SerializeField]
+    private float _maxAnimationSpeed = 4.0f;
 
-	private Vector2 _moveDirection = Vector2.zero;
-	private Rigidbody _rb;
-	private Animator _animator;
-	private float _movementSpeed;
+    private Vector2 _moveDirection = Vector2.zero;
+    private Rigidbody _rb;
+    private Animator _animator;
+    private float _movementSpeed;
 
-	//private bool _interactionPressed;
-	//private List<GameObject> _interactableGOsInRadius;
-	//private GameObject _heldObject = null;
+    [SerializeField]
+    private GameObject _playerModel;
 
-	private bool IsMoving => _moveDirection.sqrMagnitude > 0;
+    //private bool _interactionPressed;
+    //private List<GameObject> _interactableGOsInRadius;
+    //private GameObject _heldObject = null;
 
-	public void OnMove(InputAction.CallbackContext context)
-	{
-		switch (context.phase)
-		{
-			case InputActionPhase.Started:
-				break;
-			case InputActionPhase.Performed:
-			case InputActionPhase.Canceled:
-				_moveDirection = context.ReadValue<Vector2>();
-				break;
-		}
-	}
+    private bool IsMoving => _moveDirection.sqrMagnitude > 0;
 
-	public void OnFire(InputAction.CallbackContext context)
-	{
-		switch (context.phase)
-		{
-			case InputActionPhase.Started:
-				//_interactionPressed = true;
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                break;
+            case InputActionPhase.Performed:
+            case InputActionPhase.Canceled:
+                _moveDirection = context.ReadValue<Vector2>();
+                break;
+        }
+    }
 
-				//if (_heldObject != null)
-				//{
-				//	_heldObject.GetComponent<IInteractable>().OnPlayerInteract(this);
-				//	break;
-				//}
+    public void OnFire(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                //_interactionPressed = true;
 
-				////interactables are sorted by distance from player so interacting with the first one will interact with the closest
-				//if (_interactableGOsInRadius.Count > 0)
-				//{
-				//	_interactableGOsInRadius[0].GetComponent<IInteractable>().OnPlayerInteract(this);
-				//}
+                //if (_heldObject != null)
+                //{
+                //	_heldObject.GetComponent<IInteractable>().OnPlayerInteract(this);
+                //	break;
+                //}
 
-				break;
-			case InputActionPhase.Canceled:
-				//_interactionPressed = false;
-				break;
-		}
-	}
+                ////interactables are sorted by distance from player so interacting with the first one will interact with the closest
+                //if (_interactableGOsInRadius.Count > 0)
+                //{
+                //	_interactableGOsInRadius[0].GetComponent<IInteractable>().OnPlayerInteract(this);
+                //}
 
-	public void OnFire2(InputAction.CallbackContext context)
-	{
-		switch (context.phase)
-		{
-			case InputActionPhase.Started:
-				break;
-		}
-	}
+                break;
+            case InputActionPhase.Canceled:
+                //_interactionPressed = false;
+                break;
+        }
+    }
 
-	public void Pickup(GameObject obj)
-	{
-		//_heldObject = obj;
-	}
+    public void OnFire2(InputAction.CallbackContext context)
+    {
+        switch (context.phase)
+        {
+            case InputActionPhase.Started:
+                break;
+        }
+    }
 
-	public void Drop(GameObject obj)
-	{
-		//_heldObject = null;
-	}
+    public void Pickup(GameObject obj)
+    {
+        //_heldObject = obj;
+    }
 
-	// Start is called before the first frame update
-	private void Start()
-	{
-		//_interactableGOsInRadius = new List<GameObject>();
-		_rb = GetComponent<Rigidbody>();
-		_animator = GetComponentInChildren<Animator>();
-	}
+    public void Drop(GameObject obj)
+    {
+        //_heldObject = null;
+    }
 
-	// Update is called once per frame
-	private void Update()
-	{
-		if (IsMoving)
-		{
-			_movementSpeed = Mathf.Clamp(_movementSpeed + _acceleration * Time.deltaTime, 0.0f, _maxAnimationSpeed);
-			if (_animator != null)
-			{
-				_animator.SetFloat("ForwardSpeed", _movementSpeed);
-			}
-		}
-		else if (_movementSpeed > 0.0f)
-		{
-			_movementSpeed = 0.0f;
-			_animator.SetFloat("ForwardSpeed", _movementSpeed);
-		}
+    // Start is called before the first frame update
+    private void Start()
+    {
+        //_interactableGOsInRadius = new List<GameObject>();
+        _rb = GetComponent<Rigidbody>();
+        _animator = GetComponentInChildren<Animator>();
+    }
 
-		//transform.Translate(_maxSpeed * Time.deltaTime * new Vector3(_moveDirection.x, 0, _moveDirection.y));
+    // Update is called once per frame
+    private void Update()
+    {
+        if (IsMoving)
+        {
+            _movementSpeed = Mathf.Clamp(_movementSpeed + _acceleration * Time.deltaTime, 0.0f, _maxAnimationSpeed);
+            if (_animator != null)
+            {
+                _animator.SetFloat("ForwardSpeed", _movementSpeed);
+            }
+        }
+        else if (_movementSpeed > 0.0f)
+        {
+            _movementSpeed = 0.0f;
+            _animator.SetFloat("ForwardSpeed", _movementSpeed);
+        }
 
-		//	TODO: Rotate the character relative to its axis in the direction of the movement
-		//		transform.Rotate(_turnSpeed * Time.deltaTime * new Vector3(0, _moveDirection.x, 0));
+        //transform.Translate(_maxSpeed * Time.deltaTime * new Vector3(_moveDirection.x, 0, _moveDirection.y));
 
-		// remove null or inactive interactables and order them by distance from character
-		// TODO: Add highlight to closest interactable
-		//if (_interactableGOsInRadius.Count > 0)
-		//{
-		//	_interactableGOsInRadius = _interactableGOsInRadius.Where(i => i != null && i.activeInHierarchy).OrderBy(i => Vector3.Distance(i.transform.position, transform.position)).ToList();
-		//}
+        //	TODO: Rotate the character relative to its axis in the direction of the movement
+        //		transform.Rotate(_turnSpeed * Time.deltaTime * new Vector3(0, _moveDirection.x, 0));
 
-		//// move held object with the player
-		//if (_heldObject != null)
-		//{
-		//	_heldObject.transform.position = transform.position + new Vector3(0.5f, 0f, 0.5f);
-		//}
-	}
+        // remove null or inactive interactables and order them by distance from character
+        // TODO: Add highlight to closest interactable
+        //if (_interactableGOsInRadius.Count > 0)
+        //{
+        //	_interactableGOsInRadius = _interactableGOsInRadius.Where(i => i != null && i.activeInHierarchy).OrderBy(i => Vector3.Distance(i.transform.position, transform.position)).ToList();
+        //}
 
-	private void FixedUpdate()
-	{
-		Debug.Log($"Move Direction: {_moveDirection}");
+        //// move held object with the player
+        //if (_heldObject != null)
+        //{
+        //	_heldObject.transform.position = transform.position + new Vector3(0.5f, 0f, 0.5f);
+        //}
+    }
 
-		//	If there are any keys down, we should move
-		if (IsMoving)
-		{
-			Debug.Log("Moving");
-			var direction = Quaternion.Euler(0, -Vector2.SignedAngle(Vector2.up, _moveDirection), 0);
+    private void FixedUpdate()
+    {
+        //	If there are any keys down, we should move
+        if (IsMoving)
+        {
+            // Rotate player model in direction of movement
+            var direction = Quaternion.Euler(0, -Vector2.SignedAngle(Vector2.up, _moveDirection), 0);
+            var turnDirection = Quaternion.RotateTowards(transform.rotation, direction, _turnSpeed * Time.deltaTime);
+            _playerModel.transform.rotation = turnDirection.normalized;
+            _rb.MoveRotation(turnDirection.normalized);
 
-			var turnDirection = Quaternion.RotateTowards(transform.rotation, direction, _turnSpeed * Time.deltaTime);
-			_rb.MoveRotation(turnDirection.normalized);
-			//	Check whether the direction needs to be inverted, and bleed off the speed
-			var dot = Vector3.Dot(new Vector3(_moveDirection.x, 0, _moveDirection.y), transform.forward);
-			if (!Mathf.Approximately(dot, 1.0f))
-			{
-				_rb.velocity *= dot;
-			}
-			_rb.AddForce(_acceleration * Time.deltaTime * transform.forward, ForceMode.VelocityChange);
+            // Apply velocity change to rigidbody in direction of movement
+            var moveDirVec3 = new Vector3(_moveDirection.x, 0, _moveDirection.y);
+            _rb.AddForce(_acceleration * Time.deltaTime * moveDirVec3, ForceMode.VelocityChange);
 
-			//	Clamp the speed
-			if (_rb.velocity.magnitude > _maxSpeed)
-			{
-				_rb.velocity = _rb.velocity.normalized * _maxSpeed;
-			}
-		}
-		else
-		{
-			Debug.Log("stopping");
-			//	Set velocity to zero
-			_rb.velocity = Vector3.zero;
-			_rb.angularVelocity = Vector3.zero;
+            // Clamp the speed
+            if (_rb.velocity.magnitude > _maxSpeed)
+            {
+                _rb.velocity = _rb.velocity.normalized * _maxSpeed;
+            }
 
-		}
-	}
+            // If player has no input in a direction set the velocity for that direction to 0
+            if (_moveDirection.x == 0)
+            {
+                _rb.velocity = new Vector3(0, _rb.velocity.y, _rb.velocity.z);
+            }
+            if (_moveDirection.y == 0)
+            {
+                _rb.velocity = new Vector3(_rb.velocity.x, _rb.velocity.y, 0);
+            }
+        }
+        else
+        {
+            //	Set velocity to zero
+            _rb.velocity = Vector3.zero;
+            _rb.angularVelocity = Vector3.zero;
 
-	//uses OnTriggerStay rather than OnTriggerEnter as OnTriggerEnter is not called is a gameObject is set to active within the collider
-	private void OnTriggerStay(Collider other)
-	{
-		//var interactable = other.gameObject.GetComponent<IInteractable>();
-		//if (interactable != null && interactable.IsInteractable() && !_interactableGOsInRadius.Contains(other.gameObject))
-		//{
-		//	_interactableGOsInRadius.Add(other.gameObject);
-		//}
-	}
+        }
+    }
 
-	private void OnTriggerExit(Collider other)
-	{
-		//_interactableGOsInRadius.Remove(other.gameObject);
-	}
+    //uses OnTriggerStay rather than OnTriggerEnter as OnTriggerEnter is not called is a gameObject is set to active within the collider
+    private void OnTriggerStay(Collider other)
+    {
+        //var interactable = other.gameObject.GetComponent<IInteractable>();
+        //if (interactable != null && interactable.IsInteractable() && !_interactableGOsInRadius.Contains(other.gameObject))
+        //{
+        //	_interactableGOsInRadius.Add(other.gameObject);
+        //}
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        //_interactableGOsInRadius.Remove(other.gameObject);
+    }
 }
