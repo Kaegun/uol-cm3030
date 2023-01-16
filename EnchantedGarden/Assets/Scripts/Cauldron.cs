8b85b2041031e19171c8e29825798a38490b8ecc
Hamish <76265738+hbgr@users.noreply.github.com>
2023-01-16 17:26:46 +0000

    Basic cauldron implementation

---
+++ EnchantedGarden/Assets/Scripts/Cauldron.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cauldron : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _fireParticles;
+
+    [SerializeField]
+    private float _maxFuel = 10f;
+    private float _currentFuel;
+
+    [SerializeField]
+    private int _maxUses = 5;
+    private int _currentUses;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentFuel = _maxFuel;
+        _currentUses = _maxUses;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _currentFuel -= Time.deltaTime;
+        if (_currentFuel < 0 && _fireParticles.activeSelf)
+        {
+            _fireParticles.SetActive(false);
+        }
+    }
+
+    public void AddLog()
+    {
+        _currentFuel = _maxFuel;
+        if (!_fireParticles.activeSelf)
+        {
+            _fireParticles.SetActive(true);
+        }
+    }
+
+    public void AddHerb()
+    {
+        _currentUses = _maxUses;
+    }
+
+    public bool CanUseCauldron()
+    {
+        return _currentUses > 0 && _currentFuel > 0;
+    }
+
+    public void UsePotion()
+    {
+        _currentUses -= 1;
+    }
+}
