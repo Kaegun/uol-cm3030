4b3d4e2c67be158228483844949ffee7ba277c04
Johann Ungerer <johannu@azuro.co.za>
2023-02-02 00:48:17 +0200

    Added Fox Behaviour and World Events Scriptable Object.

---
+++ EnchantedGarden/Assets/Scripts/Fox/FoxBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class FoxBehaviour : MonoBehaviour
+{
+	/*
+	 * This behaviour must listen for world events, and then instruct the Fox to react to them
+	 * Test 1 - When a spirit Spawns
+	 * 
+	 * 
+	 */
+
+	[SerializeField]
+	private ScriptableWorldEventHandler _worldEvents;
+
+	// Start is called before the first frame update
+	private void Start()
+	{
+		_worldEvents.SpiritWaveSpawned += SpiritWaveSpawned;
+		_worldEvents.PlantPossessing += PlantPossessing;
+		_worldEvents.PlantPossessed += PlantPossessed;
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+
+	}
+
+	private void PlantPossessing(object sender, Vector3 e)
+	{
+		//	Alert the player
+		//	Move fox and focus the camera on the fox
+		Debug.Log($"Fox Behaviour: Plant Possessing - [{e}]");
+	}
+
+	private void PlantPossessed(object sender, Vector3 e)
+	{
+		//	Alert the player
+		//	Move fox and focus the camera on the fox
+		//	More insistent
+		//	Run to the bug sprayer
+		Debug.Log($"Fox Behaviour: Plant Possessed - [{e}]");
+	}
+
+	private void SpiritWaveSpawned(object sender, Vector3[] e)
+	{
+		//	The fox might not do much here
+		//	Could also use the camera for some of it
+		Debug.Log($"Fox Behaviour: Spirit Wave Spawned - [{e.Length}]");
+	}
+}
---
+++ EnchantedGarden/Assets/Scripts/Scriptable/ScriptableWorldEventHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Scriptable/Events/WorldEvents")]
+public class ScriptableWorldEventHandler : ScriptableEventHandler
+{
+	public event EventHandler<Vector3[]> SpiritWaveSpawned;
+	public event EventHandler<Vector3> PlantPossessed;
+	public event EventHandler<Vector3> PlantPossessing;
+
+	public void OnSpiritWaveSpawned(Vector3[] spawnLocations)
+	{
+		ExecuteEvent(SpiritWaveSpawned, spawnLocations);
+	}
+
+	public void OnPlantPossessing(Vector3 location)
+	{
+		ExecuteEvent(PlantPossessing, location);
+	}
+
+	public void OnPlantPossessed(Vector3 location)
+	{
+		ExecuteEvent(PlantPossessed, location);
+	}
+}
