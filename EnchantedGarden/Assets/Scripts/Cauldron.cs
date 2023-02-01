ba5152ab986aeb8c027f7511d9d5a0c0c0cab421
Hamish <76265738+hbgr@users.noreply.github.com>
2023-02-01 21:16:35 +0000

    Created ScriptableObject for audio clips and began updating audio playback implementation

--- EnchantedGarden/Assets/Scripts/Cauldron.cs
+++ EnchantedGarden/Assets/Scripts/Cauldron.cs
@@ -1,94 +1,128 @@
-﻿using System.Linq;
+﻿using System.Collections;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
 public class Cauldron : MonoBehaviour
 {
-	[SerializeField]
-	private GameObject _fireParticles;
+    [SerializeField]
+    private AudioSource _fireAudioSource;
+    [SerializeField]
+    private ScriptableAudioClip _fireAmbientAudio;
+    [SerializeField]
+    private ScriptableAudioClip _fireAddLogAudio;
 
-	[SerializeField]
-	private float _maxFuel = 10f;
-	private float _currentFuel;
+    [SerializeField]
+    private AudioSource _cauldronAudioSource;
+    [SerializeField]
+    private ScriptableAudioClip _cauldronCombineAudio;
+    [SerializeField]
+    private ScriptableAudioClip _cauldronBubbleAudio;
 
-	[SerializeField]
-	private int _maxUses = 5;
-	private int _currentUses;
+    [SerializeField]
+    private GameObject _fireParticles;
 
-	[SerializeField]
-	private float _combineDuration;
-	private float _combineProgress;
+    [SerializeField]
+    private float _maxFuel = 10f;
+    private float _currentFuel;
 
-	[SerializeField]
-	private GameObject _progressDots;
+    [SerializeField]
+    private int _maxUses = 5;
+    private int _currentUses;
 
-	[SerializeField]
-	private TextMeshPro _usesText;
+    [SerializeField]
+    private float _combineDuration;
+    private float _combineProgress;
 
-	// Start is called before the first frame update
-	void Start()
-	{
-		_currentFuel = _maxFuel;
-		_currentUses = _maxUses;
-	}
+    [SerializeField]
+    private GameObject _progressDots;
 
-	// Update is called once per frame
-	void Update()
-	{
-		_currentFuel -= Time.deltaTime;
-		if (_currentFuel < 0 && _fireParticles.activeSelf)
-		{
-			_fireParticles.SetActive(false);
-		}
+    [SerializeField]
+    private TextMeshPro _usesText;
 
-		//	Could we use a Trigger Collider here?
-		var combinables = Physics.OverlapSphere(transform.position, 2f).
-			Where(c => c.GetComponent<ICombinable>() != null && c.GetComponent<ICombinable>().CanBeCombined()).
-			Select(c => c.GetComponent<ICombinable>()).
-			ToList();
+    private Coroutine _fireCoroutine;
 
-		//	Could this be done in a Co-Routine?
-		if (combinables.Count > 0 && CanUseCauldron())
-		{
-			_progressDots.SetActive(true);
-			_combineProgress += Time.deltaTime;
-			if (_combineProgress >= _combineDuration)
-			{
-				UsePotion();
-				combinables[0].OnCombine();
-				_combineProgress = 0;
-			}
-		}
-		else
-		{
-			_progressDots.SetActive(false);
-			_combineProgress = 0;
-		}
-	}
+    public void AddLog()
+    {
+        // Restart FireCoroutine
+        StopCoroutine(_fireCoroutine);
+        _fireCoroutine = StartCoroutine(FireCoroutine());
+    }
 
-	public void AddLog()
-	{
-		_currentFuel = _maxFuel;
-		if (!_fireParticles.activeSelf)
-		{
-			_fireParticles.SetActive(true);
-		}
-	}
+    public void AddHerb()
+    {
+        _currentUses = _maxUses;
+        _usesText.text = $"{_currentUses}/{_maxUses}";
+    }
 
-	public void AddHerb()
-	{
-		_currentUses = _maxUses;
-		_usesText.text = $"{_currentUses}/{_maxUses}";
-	}
+    // Start is called before the first frame update
+    void Start()
+    {
+        _currentUses = _maxUses;
+        _fireCoroutine = StartCoroutine(FireCoroutine());
+        AudioController.PlayAudio(_cauldronAudioSource, _cauldronBubbleAudio);
+    }
 
-	public bool CanUseCauldron()
-	{
-		return _currentUses > 0 && _currentFuel > 0;
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        //	Could we use a Trigger Collider here?
+        var combinables = Physics.OverlapSphere(transform.position, 2f).
+            Where(c => c.GetComponent<ICombinable>() != null && c.GetComponent<ICombinable>().CanBeCombined()).
+            Select(c => c.GetComponent<ICombinable>()).
+            ToList();
 
-	public void UsePotion()
-	{
-		_currentUses -= 1;
-		_usesText.text = $"{_currentUses}/{_maxUses}";
-	}
+        //	Could this be done in a Co-Routine?
+        if (combinables.Count > 0 && CanUseCauldron())
+        {
+            _progressDots.SetActive(true);
+            _combineProgress += Time.deltaTime;
+            if (_combineProgress >= _combineDuration)
+            {
+                UsePotion();
+                combinables[0].OnCombine();
+                _combineProgress = 0;
+                StartCoroutine(CauldronCombineCoroutine());
+            }
+        }
+        else
+        {
+            _progressDots.SetActive(false);
+            _combineProgress = 0;
+        }
+    }
+
+    private bool CanUseCauldron()
+    {
+        return _currentUses > 0 && _currentFuel > 0;
+    }
+
+    private void UsePotion()
+    {
+        _currentUses -= 1;
+        _usesText.text = $"{_currentUses}/{_maxUses}";
+    }
+
+    private IEnumerator FireCoroutine()
+    {
+        _currentFuel = _maxFuel;
+        _fireParticles.SetActive(true);
+        // Play add log to fire noise
+        AudioController.PlayAudio(_fireAudioSource, _fireAddLogAudio);
+        yield return new WaitForSeconds(_fireAddLogAudio.clip.length * 0.6f);
+        // Play fire ambient noise
+        AudioController.PlayAudio(_fireAudioSource, _fireAmbientAudio);
+        yield return new WaitForSeconds(_maxFuel - _fireAddLogAudio.clip.length * 0.6f);
+        // Stop fire
+        _fireAudioSource.Stop();
+        _fireParticles.SetActive(false);
+        _currentFuel = 0;
+    }
+
+    private IEnumerator CauldronCombineCoroutine()
+    {
+        AudioController.PlayAudio(_cauldronAudioSource, _cauldronCombineAudio);
+        yield return new WaitForSeconds(_cauldronCombineAudio.clip.length * 0.8f);
+        AudioController.PlayAudio(_cauldronAudioSource, _cauldronBubbleAudio);
+    }
 }
