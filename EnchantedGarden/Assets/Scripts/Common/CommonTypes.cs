3d9ada1ea209a3d230469f5e09130d6c7b72ff8f
Johann Ungerer <johannu@azuro.co.za>
2023-02-04 22:07:28 +0200

    Added alchemy table and spirit prefabs and shader.

---
+++ EnchantedGarden/Assets/Scripts/Common/CommonTypes.cs
@@ -0,0 +1,14 @@
+﻿public class CommonTypes
+{
+	public static class Layers
+	{
+		public static readonly string Firewood = "Firewood";
+		public static readonly string TrickPlant = "Trick Plant";
+		public static readonly string Plant = "Plant";
+		public static readonly string Cauldron = "Cauldron";
+		public static readonly string Herb = "Herb";
+		public static readonly string Spirit = "Spirit";
+		public static readonly string PlantPatch = "Plant Patch";
+		public static readonly string Tool = "Tool";
+	}
+}
