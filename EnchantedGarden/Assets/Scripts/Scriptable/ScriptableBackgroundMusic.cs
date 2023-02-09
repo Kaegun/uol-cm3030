2f09ba1a5c9afc65ec715271413f49ed0547a2e2
Hamish <76265738+hbgr@users.noreply.github.com>
2023-02-09 13:33:52 +0000

    Started audio scriptable objects

---
+++ EnchantedGarden/Assets/Scripts/Scriptable/ScriptableBackgroundMusic.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewScriptableBackgroundMusic", menuName = "Scriptable/BackgroundMusic")]
+public class ScriptableBackgroundMusic : ScriptableObject
+{
+    public ScriptableAudioClip lowIntensityAudio;
+    public ScriptableAudioClip midIntensityAudio;
+    public ScriptableAudioClip highIntensityAudio;
+}
