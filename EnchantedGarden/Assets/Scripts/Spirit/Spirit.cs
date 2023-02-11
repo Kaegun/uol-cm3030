f9ec0e9447d1729b571683f4c97f22d6655cc306
Hamish <76265738+hbgr@users.noreply.github.com>
2023-02-11 21:20:59 +0000

    Further spirit searching updates

--- EnchantedGarden/Assets/Scripts/Spirit/Spirit.cs
+++ EnchantedGarden/Assets/Scripts/Spirit/Spirit.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-public class Spirit : MonoBehaviour, IInteractable
+public class Spirit : MonoBehaviour
 {
     enum SpiritState
     {
@@ -10,21 +10,18 @@ public class Spirit : MonoBehaviour, IInteractable
         StartingPossession,
         Possessing,
         Trapped,
-        Repelled
     }
 
     [SerializeField]
     private float _moveSpeed;
 
     [SerializeField]
-    private float _turnSpeed = 30f;
+    private float _turnSpeed = 10f;
 
-    [SerializeField]
-    private float _repelDuration;
+
 
     private Vector3 _moveDirection;
-    private float _repelProgress,
-        _moveTime = 0;
+    private float _moveTime = 0;
 
     private SpiritState _spiritState;
     private Plant _possessedPlant;
@@ -44,24 +41,6 @@ public class Spirit : MonoBehaviour, IInteractable
         Destroy(gameObject);
     }
 
-    public bool CanBeRepelled => _spiritState == SpiritState.Searching || _spiritState == SpiritState.Repelled;
-
-    public void Repel(Vector3 from)
-    {
-        _spiritState = SpiritState.Repelled;
-        _repelProgress = 0;
-        var direction = transform.position - from;
-        direction.y = 0;
-        _moveDirection = direction.normalized;
-    }
-
-    public bool IsInteractable => _spiritState == SpiritState.Possessing || _spiritState == SpiritState.StartingPossession;
-
-    public void OnPlayerInteract(PlayerInteractionController player)
-    {
-        Banish();
-    }
-
     //  Start is called before the first frame update
     private void Start()
     {
@@ -71,8 +50,7 @@ public class Spirit : MonoBehaviour, IInteractable
         //_renderer = GetComponent<Renderer>();
         // Track spawn position
         _spawnPos = transform.position;
-        _spiritState = SpiritState.Spawning;
-        _moveDirection = Quaternion.Euler(new Vector3(0, Random.Range(-60, 60), 0)) * transform.position.normalized * -1;
+        _spiritState = SpiritState.Spawning;        
         StartCoroutine(SpawnCoroutine());
     }
 
@@ -122,25 +100,13 @@ public class Spirit : MonoBehaviour, IInteractable
                 {
                     _possessedPlant.CompletePossession();
                     _spiritState = SpiritState.Possessing;
-                    //  use current position to approximate direction fastest to edge of forest
-                    _moveDirection = transform.position.normalized;
+                    _moveDirection = (_spawnPos - transform.position).normalized;
                 }
 
                 break;
             case SpiritState.Possessing:
-                //	TODO: Ensure all movement is using common Move method
-                transform.position += _moveSpeed * Time.deltaTime * _moveDirection;
+                Move();
                 _possessedPlant.transform.position = transform.position;
-                break;
-            case SpiritState.Repelled:
-                _repelProgress += Time.deltaTime;
-                if (_repelProgress > _repelDuration)
-                {
-                    _spiritState = SpiritState.Searching;
-                }
-
-                Move(2.5f);
-
                 break;
             default:
                 break;
@@ -158,7 +124,11 @@ public class Spirit : MonoBehaviour, IInteractable
 
     private void Move(float speedFactor = 1.0f)
     {
-        //	TODO: Place common movement code here        
+        //	TODO: Place common movement code here
+        // Prevent movement on y axis
+        // TODO: Replace _moveDirection with Vector2?
+        _moveDirection.y = 0;
+        _moveDirection = _moveDirection.normalized;
         transform.rotation = transform.rotation.RotateTowards(transform.position, transform.position + _moveDirection, _turnSpeed * Time.deltaTime);
         transform.position += _moveSpeed * Time.deltaTime * _moveDirection * speedFactor;
 
@@ -178,10 +148,11 @@ public class Spirit : MonoBehaviour, IInteractable
 
     private IEnumerator SpawnCoroutine()
     {
+        _moveDirection = Quaternion.Euler(new Vector3(0, Random.Range(-60, 60), 0)) * transform.position.normalized * -1;
         float t = 0;
-        while (t < 1.5)
+        while (t < 2f)
         {
-            Move();
+            Move(0.5f);
             t += Time.deltaTime;
             yield return new WaitForFixedUpdate();
         }
@@ -190,25 +161,26 @@ public class Spirit : MonoBehaviour, IInteractable
     }
 
     private IEnumerator SearchCoroutine()
-    {        
+    {
         float searchDuration = 0;
         while (_spiritState == SpiritState.Searching)
         {
+            // TODO: Update layer mask based on search duration
             int layerMask = CommonTypes.LayerAsLayerMask(CommonTypes.Layers.Plant);
             // While target plant is not a valid target, spin and use raycasts to search for new valid target
             if (_targetPlant == null || !_targetPlant.CanBePossessed)
             {
                 float t = 0;
-                while (t < 3 && (_targetPlant == null || !_targetPlant.CanBePossessed))
+                while (t < 2)
                 {
                     if (Physics.Raycast(transform.position, transform.rotation * Vector3.forward, out RaycastHit hit, 100f, layerMask)
                         && hit.collider.TryGetComponent(out Plant hitPlant)
                         && hitPlant.CanBePossessed)
                     {
                         Debug.DrawRay(transform.position, (transform.rotation * Vector3.forward).normalized * hit.distance, Color.yellow);
-                        //_targetPlant = hitPlant;
+                        _targetPlant = hitPlant;
                     }
-                    float bounce = Mathf.PingPong(Time.time, 2f) - 1f;
+                    float bounce = Mathf.PingPong(Time.time, 1.6f) - 0.8f;
                     transform.rotation = transform.rotation.RotateTowards(transform.position,
                         transform.position + transform.rotation * Vector3.right * bounce,
                         Time.deltaTime * _turnSpeed / 8);
@@ -217,7 +189,7 @@ public class Spirit : MonoBehaviour, IInteractable
                 }
                 _moveDirection = Quaternion.Euler(new Vector3(0, Random.Range(-60, 60), 0)) * transform.position.normalized * -1;
                 t = 0;
-                while (t < 3)
+                while (t < 3 && (_targetPlant == null || !_targetPlant.CanBePossessed))
                 {
                     Move();
                     t += Time.deltaTime;
@@ -236,39 +208,36 @@ public class Spirit : MonoBehaviour, IInteractable
 
     private void OnTriggerEnter(Collider other)
     {
-        //Debug.Log($"Spirt.OnTriggerEnter: {other.gameObject.name}");
         //	Handle plants
-        /*if (other.gameObject.IsLayer(CommonTypes.Layers.Plant)
+        if (other.gameObject.IsLayer(CommonTypes.Layers.Plant)
             && other.TryGetComponent(out _possessedPlant)
-            && _possessedPlant.CanBePossessed)
+            && _possessedPlant.CanBePossessed
+            && _spiritState == SpiritState.Searching)
         {
             //  handle normal plants
+            StopAllCoroutines();
             _possessedPlant.StartPossession();
-            transform.position = _possessedPlant.transform.position;
+            transform.position = new Vector3(_possessedPlant.transform.position.x, transform.position.y, _possessedPlant.transform.position.z);
             _spiritState = SpiritState.StartingPossession;
             DeactivateBody();
-
-        }
-        else if (other.gameObject.IsLayer(CommonTypes.Layers.TrickPlant)
-            && other.TryGetComponent(out TrickPlant trickPlant)
-            && trickPlant.CanTrapSpirit)
-        {
-            //  handle trick plants
-            trickPlant.TrapSpirit(this);
-            DeactivateBody();
-            _spiritState = SpiritState.Trapped;
-            transform.position = trickPlant.transform.position;
         }
         else if (other.gameObject.IsLayer(CommonTypes.Layers.Forest) && IsPossessingPlant)
         {
             //	handle - we're off the edge of the map Jim
             StealPossessedPlant();
-        }*/
-    }
+        }
 
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.magenta;
-        //Gizmos.DrawLine(transform.position, transform.position + 10 * (transform.rotation * Vector3.forward));
+        // TODO: Rework trick plants
+        //else if (other.gameObject.IsLayer(CommonTypes.Layers.TrickPlant)
+        //    && other.TryGetComponent(out TrickPlant trickPlant)
+        //    && trickPlant.CanTrapSpirit)
+        //{
+        //    //  handle trick plants
+        //    trickPlant.TrapSpirit(this);
+        //    DeactivateBody();
+        //    _spiritState = SpiritState.Trapped;
+        //    transform.position = trickPlant.transform.position;
+        //}
+
     }
 }
