cbc0952fddf6acd1cb00b8f708912bfe3d200787
Johann Ungerer <johannu@azuro.co.za>
2023-02-13 09:01:36 +0200

    First publish for playtesting.

--- EnchantedGarden/Assets/Scripts/Common/CommonTypes.cs
+++ EnchantedGarden/Assets/Scripts/Common/CommonTypes.cs
@@ -1,35 +1,35 @@
 public static class CommonTypes
 {
-    public enum Layers
-    {
-        Default = 0,
-        TransparentFX = 1,
-        IgnoreRaycast = 2,
-        Water = 4,
-        UI = 5,
-        Firewood = 8,
-        TrickPlant = 9,
-        Plant = 10,
-        Cauldron = 11,
-        Herb = 12,
-        Spirit = 13,
-        PlantPatch = 14,
-        Tool = 15,
-        Player = 16,
-        Forest = 17,
-        SpiritWall = 18,
-    }
+	public enum Layers
+	{
+		Default = 0,
+		TransparentFX = 1,
+		IgnoreRaycast = 2,
+		Water = 4,
+		UI = 5,
+		Firewood = 8,
+		TrickPlant = 9,
+		Plant = 10,
+		Cauldron = 11,
+		Herb = 12,
+		Spirit = 13,
+		PlantPatch = 14,
+		Tool = 15,
+		Player = 16,
+		Forest = 17,
+		SpiritWall = 18,
+	}
 
-    public static class AnimatorActions
-    {
-        public const string ForwardSpeed = "ForwardSpeed";
-        public const string Alert = "Alert";
-    }
+	public static class AnimatorActions
+	{
+		public const string ForwardSpeed = "ForwardSpeed";
+		public const string Alert = "Alert";
+	}
 
 	public static class Scenes
 	{
 		public const string UI = "UI Overlay";
 		public const string GameOver = "Game Over";
-		public const string Level1 = "ju-beta-1";   //	TODO: Name properly
+		public const string Level1 = "Template";   //	TODO: Name properly
 	}
 }
