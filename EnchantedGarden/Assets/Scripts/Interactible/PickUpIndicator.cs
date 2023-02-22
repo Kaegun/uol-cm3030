02218fa2e31c9830b0dfc8c386d80b5e94394dc1
Hamish <76265738+hbgr@users.noreply.github.com>
2023-02-22 15:55:00 +0000

    Questionable implementation of new carry icons

--- EnchantedGarden/Assets/Scripts/Interactible/PickUpIndicator.cs
+++ EnchantedGarden/Assets/Scripts/Interactible/PickUpIndicator.cs
@@ -5,6 +5,9 @@ public class PickUpIndicator : MonoBehaviour
 	[SerializeField]
 	private SpriteRenderer _icon;
 
+	[SerializeField]
+	private SpriteRenderer _secondaryIcon;
+
 	private Camera _camera;
 
 	public void SetIcon(Sprite icon)
@@ -12,9 +15,19 @@ public class PickUpIndicator : MonoBehaviour
 		_icon.sprite = icon;
 	}
 
+	public void SetSecondaryIcon(Sprite icon)
+    {
+		_secondaryIcon.sprite = icon;
+	}
+
 	public void SetIconColor(Color color)
 	{
 		_icon.color = color;
+	}
+
+	public void SetSecondaryIconColor(Color color)
+	{
+		_secondaryIcon.color = color;
 	}
 
 	public bool Active => gameObject.activeSelf;
