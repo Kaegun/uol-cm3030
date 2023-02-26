3822f7da262f5431a6a22ac24f1827d85b537293
Johann Ungerer <johannu@azuro.co.za>
2023-02-26 13:26:49 +0200

    Added Victory UI script. Updated Overlay to fit better on screen.

--- EnchantedGarden/Assets/Scripts/Systems/ScoreController.cs
+++ EnchantedGarden/Assets/Scripts/Systems/ScoreController.cs
@@ -1,16 +1,25 @@
-﻿public class ScoreController
+﻿using UnityEngine;
+
+public class ScoreController
 {
 	private ScriptableWorldEventHandler _events;
 	private float _score = 0;
+	private int _rating;
 
 	public float Score => _score;
 
+	public int Rating => _rating;
+
 	public ScoreController(ScriptableWorldEventHandler events)
 	{
+		Debug.Log("Score Controller Loading");
+
 		_events = events;
 
 		_events.SpiritBanished += SpiritBanished;
 		_events.SpiritWallBanished += SpiritWallBanished;
+
+		Debug.Log("Score Controller Loaded");
 	}
 
 	public int CalculateRating(int numPlantsRemaining, int maxPlants)
@@ -19,17 +28,19 @@
 		var plantRatio = numPlantsRemaining / (float)maxPlants;
 		_score *= plantRatio > 0.8f ? 3 : plantRatio > 0.5f ? 2 : 1;
 
-		return _score > 10000 ? 3 : _score > 5000 ? 2 : 1;
+		return _rating = _score > 10000 ? 3 : _score > 5000 ? 2 : 1;
 	}
 
 	private void SpiritBanished(object sender, Spirit e)
 	{
+		Debug.Log("ScoreController.SpiritBanished");
 		_score += CommonTypes.ScoreValues.BanishedSpirit;
 		_events.OnScore(CommonTypes.ScoreValues.BanishedSpirit);
 	}
 
 	private void SpiritWallBanished(object sender, SpiritWall e)
 	{
+		Debug.Log("ScoreController.SpiritWallBanished");
 		_score += CommonTypes.ScoreValues.BanishedWall;
 		_events.OnScore(CommonTypes.ScoreValues.BanishedWall);
 	}
---
+++ EnchantedGarden/Assets/Scripts/UI/UiVictory.cs
@@ -0,0 +1,35 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiVictory : MonoBehaviour
+{
+	[Header("Stars")]
+	[SerializeField]
+	private Image[] _stars = new Image[3];
+
+	[SerializeField]
+	private Sprite _starFilled;
+
+	[SerializeField]
+	private Sprite _starEmpty;
+
+	[SerializeField]
+	private TMP_Text _scoreText;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		//  Get Score from Game Manager
+		var score = GameManager.Instance.Score;
+
+		//  Set Stars
+		for (int i = 0; i < 3; i++)
+		{
+			_stars[i].sprite = score.Rating > i ? _starFilled : _starEmpty;
+		}
+
+		//  Set Score
+		_scoreText.text = $"{score.Score:# ##0}";
+	}
+}
